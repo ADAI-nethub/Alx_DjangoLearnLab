d9fa3480e1681b364d8e80298fde31378be4f727
ADAI-nethub <atomosdubaako@gmail.com>
2025-07-23 18:33:25 +0100

    views.py imports ExampleForm correctly

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -1,11 +1,7 @@
-# bookshelf/urls.py
-
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('books/', views.book_list, name='book_list'),
-    path('books/create/', views.create_book, name='create_book'),
-    path('books/<int:book_id>/edit/', views.edit_book, name='edit_book'),
-    path('books/<int:book_id>/delete/', views.delete_book, name='delete_book'),
+    path('admin/', admin.site.urls),
+    path('books/', include('bookshelf.urls')),  # ✅ Add this line
 ]
