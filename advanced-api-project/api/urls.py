056594b324864da7290bf4eecfd9fa115e8f83cd
ADAI-nethub <atomosdubaako@gmail.com>
2025-08-05 15:15:52 +0100

    api/urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -7,10 +7,16 @@ from .views import (
     BookDeleteView
 )
 
+from .views import DefaultBookUpdateView, DefaultBookDeleteView  # You must create these views
+
 urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
     path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
     path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    
+    # NEW:
+    path('books/update/', DefaultBookUpdateView.as_view(), name='book-update-no-pk'),
+    path('books/delete/', DefaultBookDeleteView.as_view(), name='book-delete-no-pk'),
 ]
