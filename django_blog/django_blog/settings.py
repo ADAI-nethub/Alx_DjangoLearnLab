a84560a51d28a3803260cb72223b58130653473c
ADAI-nethub <atomosdubaako@gmail.com>
2025-08-11 19:02:42 +0100

    user, port

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_database_name',
+        'USER': 'your_db_username',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'localhost',  # Or your database server address
+        'PORT': '5432',       # Default PostgreSQL port
     }
 }
+
 
 
 # Password validation
