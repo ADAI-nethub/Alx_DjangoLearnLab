3abdf018197a85758e60bef4d3d1a431f74a1a14
ADAI-nethub <atomosdubaako@gmail.com>
2025-08-13 11:14:51 +0100

    Implement authentication and clean duplicates

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
