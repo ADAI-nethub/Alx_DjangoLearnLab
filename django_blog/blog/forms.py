f452feb862cd2898e52abc4877426458c85f0397
ADAI-nethub <atomosdubaako@gmail.com>
2025-08-14 18:00:50 +0100

    Untracked files:

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile
-from .models import Post
+from .models import Profile, Post, Comment
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 4, 'class': 'form-control'}),
+        }
 
 class PostForm(forms.ModelForm):
     class Meta:
