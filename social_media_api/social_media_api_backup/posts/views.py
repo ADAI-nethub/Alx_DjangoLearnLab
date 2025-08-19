97b9a499c9ab27fb86f7857ce5ae1cac29b6d209
ADAI-nethub <atomosdubaako@gmail.com>
2025-08-19 11:52:35 +0100

    social_media_api/urls.py

---
+++ social_media_api/social_media_api_backup/posts/views.py
@@ -0,0 +1,39 @@
+from rest_framework import viewsets, permissions, filters
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from accounts.models import User
+from django_filters.rest_framework import DjangoFilterBackend
+
+class PostViewSet(viewsets.ModelViewSet):
+    # ... existing code ...
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['author']
+    search_fields = ['title', 'content']
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def get_queryset(self):
+        post_id = self.kwargs['post_pk']
+        return Comment.objects.filter(post_id=post_id).order_by('created_at')
+
+    def perform_create(self, serializer):
+        post = Post.objects.get(pk=self.kwargs['post_pk'])
+        serializer.save(author=self.request.user, post=post)
+
+
