6b9418045f572af20ac070ed241e6c05d70ea88f
ADAI-nethub <atomosdubaako@gmail.com>
2025-08-19 12:42:32 +0100

    social_media_api/urls.py

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/auth/', include('accounts.urls')),  # For authentication
-    path('api/', include('posts.urls')),  # For posts and comments
+    path("admin/", admin.site.urls),
+    path("api/auth/", include("accounts.urls")),
+    path("api/", include("posts.urls")),
 ]
