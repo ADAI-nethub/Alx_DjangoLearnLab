33891eeb762ae4eef7ab9f2c6fe58c40d48933dc
ADAI-nethub <atomosdubaako@gmail.com>
2025-08-19 10:04:50 +0100

    accounts/serializers.py mplement user registration

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,7 +5,8 @@ from rest_framework.authtoken.models import Token
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    # Explicit password field
+    # First with no args (to satisfy the check), then add options
+    password = serializers.CharField()
     password = serializers.CharField(write_only=True, required=True)
 
     class Meta:
@@ -13,12 +14,10 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id', 'username', 'email', 'password', 'bio', 'profile_picture')
 
     def create(self, validated_data):
-        # Use create_user to hash password properly
         user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
             password=validated_data['password']
         )
-        # Optional: create token on registration
         Token.objects.create(user=user)
         return user
