ca9d696f0eb5139897480959a19707d1e49a88f9
ADAI-nethub <atomosdubaako@gmail.com>
2025-08-19 14:00:59 +0100

    generics.get_object_or_404

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,22 +1,36 @@
-from rest_framework import viewsets, permissions, filters
-from .models import Post, Comment
+from rest_framework import viewsets, permissions, filters, generics
+from .models import Post, Comment, Like
 from .serializers import PostSerializer, CommentSerializer
 from accounts.models import User
 from django_filters.rest_framework import DjangoFilterBackend
-
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from .models import Like
 from notifications.models import Notification
 
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
+
 class PostViewSet(viewsets.ModelViewSet):
-    # ... existing code ...
-    
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['author']
+    search_fields = ['title', 'content']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
     @action(detail=True, methods=['post'], permission_classes=[permissions.IsAuthenticated])
     def like(self, request, pk=None):
-        post = self.get_object()
+        post = generics.get_object_or_404(Post, pk=pk)
         like, created = Like.objects.get_or_create(user=request.user, post=post)
-        
+
         if created:
             # Create notification
             Notification.objects.create(
@@ -27,10 +41,10 @@ class PostViewSet(viewsets.ModelViewSet):
             )
             return Response({'status': 'post liked'})
         return Response({'status': 'already liked'}, status=400)
-    
+
     @action(detail=True, methods=['post'], permission_classes=[permissions.IsAuthenticated])
     def unlike(self, request, pk=None):
-        post = self.get_object()
+        post = generics.get_object_or_404(Post, pk=pk)
         try:
             like = Like.objects.get(user=request.user, post=post)
             like.delete()
@@ -38,25 +52,6 @@ class PostViewSet(viewsets.ModelViewSet):
         except Like.DoesNotExist:
             return Response({'status': 'not liked'}, status=400)
 
-class PostViewSet(viewsets.ModelViewSet):
-    # ... existing code ...
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    filterset_fields = ['author']
-    search_fields = ['title', 'content']
-
-class IsAuthorOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.author == request.user
-
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('-created_at')
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
@@ -67,7 +62,5 @@ class CommentViewSet(viewsets.ModelViewSet):
         return Comment.objects.filter(post_id=post_id).order_by('created_at')
 
     def perform_create(self, serializer):
-        post = Post.objects.get(pk=self.kwargs['post_pk'])
+        post = generics.get_object_or_404(Post, pk=self.kwargs['post_pk'])
         serializer.save(author=self.request.user, post=post)
-
-
