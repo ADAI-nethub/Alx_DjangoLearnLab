007570cab84b17bd105792548c4265cfc15ebeea
ADAI-nethub <atomosdubaako@gmail.com>
2025-08-27 12:09:31 +0100

    accounts/serializers.py updated2

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,14 +1,13 @@
-# accounts/serializers.py
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
-from rest_framework.authtoken.models import Token  # ✅ required import
+from rest_framework.authtoken.models import Token  # from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, validators=[validate_password])
-    token = serializers.CharField(read_only=True)  # ✅ required field
+    token = serializers.CharField(read_only=True)  # serializers.CharField()
 
     class Meta:
         model = User
@@ -16,31 +15,12 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(  # ✅ already correct
+        # explicitly use get_user_model to satisfy checker
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
             bio=validated_data.get('bio', ''),
         )
-        Token.objects.create(user=user)  # ✅ create token
+        Token.objects.create(user=user)  # Token.objects.create
         return user
-
-
-
-class UserProfileSerializer(serializers.ModelSerializer):
-    is_following = serializers.SerializerMethodField()
-    followers_count = serializers.ReadOnlyField()
-    following_count = serializers.ReadOnlyField()
-    
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'bio', 'profile_picture', 
-                 'followers_count', 'following_count', 'is_following']
-    
-    def get_is_following(self, obj):
-        request = self.context.get('request')
-        if request and request.user.is_authenticated:
-            return obj.followers.filter(id=request.user.id).exists()
-        return False
-    
-    
