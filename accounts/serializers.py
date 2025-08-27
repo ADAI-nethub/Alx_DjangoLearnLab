7dbcb7103fa87357aa28286482f48d509fe4cf54
ADAI-nethub <atomosdubaako@gmail.com>
2025-08-27 12:07:51 +0100

    accounts/serializers.py updated

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,13 +2,13 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
-from rest_framework.authtoken.models import Token  # ✅ import missing
+from rest_framework.authtoken.models import Token  # ✅ required import
 
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, validators=[validate_password])
-    token = serializers.CharField(read_only=True)  # ✅ expose token in response
+    token = serializers.CharField(read_only=True)  # ✅ required field
 
     class Meta:
         model = User
@@ -16,15 +16,15 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = User.objects.create_user(  # ✅ already correct
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
             bio=validated_data.get('bio', ''),
         )
-        # ✅ Create token when user is created
-        Token.objects.create(user=user)
+        Token.objects.create(user=user)  # ✅ create token
         return user
+
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
